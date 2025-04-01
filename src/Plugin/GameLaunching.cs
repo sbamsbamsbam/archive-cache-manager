7d43a981f80017c81c8e664e173bfcc68905089d
sbamsbamsbam <128758730+sbamsbamsbam@users.noreply.github.com>
2025-04-01 21:48:43 +0200

    Update GameLaunching.cs

--- src/Plugin/GameLaunching.cs
+++ src/Plugin/GameLaunching.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Unbroken.LaunchBox.Plugins;
 using Unbroken.LaunchBox.Plugins.Data;
@@ -10,17 +10,12 @@ namespace ArchiveCacheManager
 {
     class GameLaunching : IGameLaunchingPlugin
     {
-        private static bool cacheManagerActive = false;
         private static readonly string tempArchivePath = PathUtils.GetTempArchivePath();
 
         public void OnAfterGameLaunched(IGame game, IAdditionalApplication app, IEmulator emulator)
         {
-            if (cacheManagerActive)
-            {
-                Logger.Log("Game started, cleaning up 7-Zip folder.");
-                Restore7z();
-                LaunchBoxDataBackup.RestoreAllSettings();
-            }
+            Logger.Log("Game started.");
+            LaunchBoxDataBackup.RestoreAllSettings();
         }
 
         private static bool UseArchiveCacheManager(IGame game, IAdditionalApplication app, IEmulator emulator)
@@ -176,11 +171,8 @@ namespace ArchiveCacheManager
                 }
 
                 LaunchBoxDataBackup.RestoreAllSettings();
-                cacheManagerActive = true;
 
                 GameInfo gameInfo = SaveGameInfo(game, app, emulator);
-
-                Replace7z();
 
                 // This code block is responsible for making temporary settings changes, to support different game launch scenarios.
                 // All settings are reverted after a game has launched, or after 5 seconds.
@@ -227,8 +219,6 @@ namespace ArchiveCacheManager
 
         public void OnGameExited()
         {
-            if (cacheManagerActive)
-            {
                 // The temp path will be empty most times, but in the case where files were extracted to temp (the cache folder
                 // doesn't exist or the archive is too small) AND a file priority has been applied, non priority files won't be
                 // removed from the temp folder (as LB doesn't know about them). Force clear this folder on game exit.
@@ -238,91 +228,7 @@ namespace ArchiveCacheManager
                 {
                     PluginHelper.LaunchBoxMainViewModel.RefreshData();
                 }
-
-                cacheManagerActive = false;
-            }
         }
 
-        /// <summary>
-        /// Replaces 7z.exe with Archive Cache Manager, renaming 7z.exe to 7-zip.exe.
-        /// </summary>
-        public static void Replace7z()
-        {
-            bool copyError = false;
-            string pluginRootPath = PathUtils.GetPluginRootPath();
-            string plugin7zRootPath = PathUtils.GetPlugin7zRootPath();
-            string launchBox7zRootPath = PathUtils.GetLaunchBox7zRootPath();
-            Dictionary<string, string> paths = new Dictionary<string, string>();
-
-            paths.Add(Path.Combine(pluginRootPath, "ArchiveCacheManager.Core.dll"), Path.Combine(launchBox7zRootPath, "ArchiveCacheManager.Core.dll"));
-            paths.Add(Path.Combine(pluginRootPath, "INIFileParser.dll"), Path.Combine(launchBox7zRootPath, "INIFileParser.dll"));
-            paths.Add(Path.Combine(pluginRootPath, "ArchiveCacheManager.exe"), Path.Combine(launchBox7zRootPath, "7z.exe"));
-            paths.Add(Path.Combine(plugin7zRootPath, "7z.exe.original"), Path.Combine(launchBox7zRootPath, "7-zip.exe"));
-
-            foreach (var path in paths)
-            {
-                try
-                {
-                    File.Copy(path.Key, path.Value, true);
-                }
-                catch (Exception e)
-                {
-                    copyError = true;
-                    Logger.Log(e.ToString(), Logger.LogLevel.Exception);
-                    break;
-                }
-            }
-
-            if (copyError)
-            {
-                Logger.Log("Error setting up Archive Cache Manager, cleaning up 7-Zip folder.");
-                cacheManagerActive = false;
-                Restore7z();
-            }
-        }
-
-        /// <summary>
-        /// Restores the original 7z.exe, and removes Archive Cache Manager from the ThirdParty\7-Zip folder.
-        /// </summary>
-        /// TODO: There's a potential race condition where a game may be extracting, and as the extraction
-        /// completes and the replacement 7z.exe process exits, LB runs a data backup. The data backup runs
-        /// under the replacement 7z.exe, but then this function runs and begins reverting files, including
-        /// the renamed 7-zip.exe. 7z.exe will attempt to run 7-zip.exe, but throw an exception because it
-        /// is missing.
-        public static void Restore7z()
-        {
-            string plugin7zRootPath = PathUtils.GetPlugin7zRootPath();
-            string launchBox7zRootPath = PathUtils.GetLaunchBox7zRootPath();
-
-            string[] paths = new string[] { Path.Combine(launchBox7zRootPath, "ArchiveCacheManager.Core.dll"),
-                                            Path.Combine(launchBox7zRootPath, "INIFileParser.dll"),
-                                            Path.Combine(launchBox7zRootPath, "7-zip.exe"),
-                                            PathUtils.GetGameInfoPath() };
-
-            try
-            {
-                File.Copy(Path.Combine(plugin7zRootPath, "7z.exe.original"), Path.Combine(launchBox7zRootPath, "7z.exe"), true);
-                File.Copy(Path.Combine(plugin7zRootPath, "7z.dll.original"), Path.Combine(launchBox7zRootPath, "7z.dll"), true);
-            }
-            catch (Exception e)
-            {
-                Logger.Log(e.ToString(), Logger.LogLevel.Exception);
-            }
-
-            foreach (string path in paths)
-            {
-                try
-                {
-                    if (File.Exists(path))
-                    {
-                        File.Delete(path);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Logger.Log(e.ToString(), Logger.LogLevel.Exception);
-                }
-            }
-        }
     }
 }
